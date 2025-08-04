3c6bc89b8bd7bcaf5ba0bdadf4455ba68537df23
Mohammad Bilal <mohdbilal16@gmail.com>
2025-08-04 13:17:37 +0530

    update clarity code

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,11 +29,11 @@ export default function RootLayout({
           type="text/javascript"
           dangerouslySetInnerHTML={{
             __html: `
-          (function(c,l,a,r,i,t,y){
-                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "r6ejow727a");
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "spgq6hg9v3");
           `,
           }}
         />
